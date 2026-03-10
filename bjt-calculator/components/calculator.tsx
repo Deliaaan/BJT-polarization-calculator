ac2deea079d35ac1e7b8c1a25c5b24ab0d05c2a8
Diego nava <delian.navaj@gmail.com>
2026-03-10 16:11:47 -0600

    Vceq

--- bjt-calculator/components/calculator.tsx
+++ bjt-calculator/components/calculator.tsx
@@ -3,60 +3,112 @@
 import React, { useState } from "react";
 
 export default function Calculator() {
-	const [operation, setOperation] = useState("");
-	const [num1, setNum1] = useState("");
-	const [num2, setNum2] = useState("");
-	const [result, setResult] = useState("0");
+
+  // switch para cambiar de polarizacion
+	const [operation, setOperation] = useState("fija");
+
+  // variables de entrada
+	const [vcc, setVcc] = useState(0);
+	const [resb, setResb]   = useState(0);
+  const [resc, setResc]   = useState(0);
+  const [beta, setBeta] = useState(0);
+  const vbe: number = 0.7;
+
+  // variables de salida
+	const [icq, setIcq] = useState(0);
+	const [vceq, setVceq] = useState(0);
 
 
   const checkValidNum = ( num: number ) => {
     if (isNaN(num)) {
-    setResult("enter valid numbers")
+    alert("Porfavor inserta valores validos")
    }else {
-    setResult(num.toString())
+    setIcq(num)
    }
   }
-	const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-		e.preventDefault();
-    console.log(operation)
 
-		if (operation === "suma") {
-			const suma = parseFloat(num1) + parseFloat(num2);
-			return checkValidNum(suma);
+  const calcIQC = (
+    vcc: number, 
+    resb: number,
+    beta: number,
+  ) => {
+		resb * 1000
+    const ib = (vcc - vbe)/ resb;
+    const icq = beta * ib;
+    return icq;
+  }
+
+	const calcVceq = (
+		vcc: number,
+		resc: number,
+		icq: number
+	) => {
+		resc * 1000
+		const vecq = vcc - (icq * resc)
+		return vecq
+	}
+ 
+
+	const handleOperation = (e: React.SyntheticEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (operation === "fija") {
+			setIcq(calcIQC(vcc, resb, resc))
+			setVceq(calcVceq(vcc, vceq, icq))
 		} else if (operation === "resta"){
-			const resta = parseFloat(num1) - parseFloat(num2);
-			return checkValidNum(resta);
+			alert("opcion 2")
 		} else {
-
+      alert("bad ending")
     }
 	};
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleOperation}>
+				<p>Vcc</p>
 				<input
 					type="number"
-					name="num1"
-					value={num1}
-					onChange={(e) => setNum1(e.target.value)}
+					name="Vcc"
+          placeholder="Vcc"
+					value={vcc}
+					onChange={(e) => setVcc(e.target.valueAsNumber)}
 				/>
+				<p>ResB</p>
 				<input
 					type="number"
-					name="num2"
-					value={num2}
-					onChange={(e) => setNum2(e.target.value)}
+					name="resB"
+          placeholder="Resistencia base"
+					value={resb}
+					onChange={(e) => setResb(e.target.valueAsNumber)}
 				/>
-				<button type="submit">hola</button>
+				<p>ResC</p>
+        <input
+					type="number"
+					name="resC"
+          placeholder="Resistencia colector"
+					value={resc}
+					onChange={(e) => setResc(e.target.valueAsNumber)}
+				/>
+				<p>Beta</p>
+        <input
+					type="number"
+					name="Beta"
+          placeholder="Beta"
+					value={beta}
+					onChange={(e) => setBeta(e.target.valueAsNumber)}
+				/>
+				<button type="submit"> Calcular </button>
 			</form>
 			<div>
-				<h1>Result: {result}</h1>
+				<h1>Vbe: {vbe}</h1>
+				<h1>Icq: {icq}</h1> <b/>
+				<h1>Vceq: {vceq}</h1>
 			</div>
-
       <div>
         <div>
           <h2>Operacion: {operation}</h2>
         </div>
-        <button onClick={ () => setOperation("suma")}>Suma</button>
+        <button onClick={ () => setOperation("fija")}> Polarizacion Fija </button>
         <button onClick={ () => setOperation("resta")}>Resta</button>
       </div>
 		</div>
